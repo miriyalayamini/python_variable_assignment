d4aa15ab6bfd3f71cc011b0a6361e300b28d20fd
miriyalayamini <miriyalayamini81@gmail.com>
2026-06-25 08:33:34 +0530

    Add files via upload

--- yamini_PYTHONTASK1.py.py
+++ yamini_PYTHONTASK1.py.py
@@ -1,5 +1,5 @@
 "Declare and initialize five different variables with distinct data types (e.g., integer, float, string, boolean, list)."
-'''s_id = 101
+s_id = 101
 s_cgpa = 8.1
 s_name = 'yamini'
 is_placed = True
@@ -8,18 +8,18 @@ print('s_id is' ,type(s_id))
 print('s_cgpa is' ,type(s_cgpa))
 print('s_name is' ,type(s_name))
 print('is_placed is' ,type(is_placed))
-print('skills is', type(skills))'''
+print('skills is', type(skills))
 
 "Write a Python function that takes two variables as input, swaps their values, and returns the swapped values."
-''' def swap(a,b):
+def swap(a,b):
     return b,a
 n1 = 10
 n2 = 20
 print('num1 =', n2)
-print('num2 =', n1)'''
+print('num2 =', n1)
 
 "Create a simple calculator program that uses variables to store user input (e.g., numbers and operators) and calculate the result based on the operator."
-'''a=int(input())
+a=int(input())
 b=int(input())
 op=input('enter operator(+,-,*,/)')
 if op == '+':
@@ -35,14 +35,14 @@ elif op == '/':
         res = 'division by zero is not possible'
 else:
     res = 'invalid operator'
-print(res) '''
+print(res) 
 
 "Use variables to generate a formatted string that includes user-provided information (e.g., name, age, location)."
-'''name = 'Yamini'
+name = 'Yamini'
 age = 20
 location = 'Hyderabad'
 res = f"My name is {name}. I am {age} years old and I live in {location}."
-print(res)'''
+print(res)
 
 "Demonstrate the use of variable scope by creating a nested function that accesses and modifies a variable declared in the outer function."
 def outer():
